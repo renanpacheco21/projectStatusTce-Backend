496668dc68e42d1e8aa2fa733a4fa4e66f100655
renanpacheco21 <renan1025>
2021-09-11 10:05:38 -0300

    Renan

--- src/main/java/com/betha/statustce/statustce/resource/MunicipioController.java
+++ src/main/java/com/betha/statustce/statustce/resource/MunicipioController.java
@@ -2,22 +2,44 @@ package com.betha.statustce.statustce.resource;
 
 
 import com.betha.statustce.statustce.model.Estado;
+import com.betha.statustce.statustce.model.Municipio;
+import com.betha.statustce.statustce.model.Pais;
 import com.betha.statustce.statustce.repository.EstadoRepository;
+import com.betha.statustce.statustce.repository.MunicipioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/municipios")
 public class MunicipioController {
     @Autowired
-    private EstadoRepository repository;
+    private MunicipioRepository repository;
 
     @GetMapping
-    public List<Estado> getMunicipio(){
+    public List<Municipio> getMunicipio(){
         return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Municipio getEstadosId(@PathVariable(value = "id") Long municipioId) throws EntityNotFoundException {
+        Municipio municipioFind = repository.findById(municipioId).orElseThrow(() -> new EntityNotFoundException("Município não encontado com o ID"+ municipioId));
+        return municipioFind;
+    }
+
+    @PostMapping
+    public Municipio create(@RequestBody Municipio municipio){
+        return repository.save(municipio);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable(value = "id") Long municipioId) throws EntityNotFoundException{
+        Municipio municipioFind = repository.findById(municipioId).orElseThrow(() -> new EntityNotFoundException("Municipio não encontrado com o ID::"+ municipioId));
+        repository.delete(municipioFind);
+        return ResponseEntity.noContent().build();
     }
 }
