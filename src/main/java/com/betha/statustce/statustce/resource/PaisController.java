bf836e32baddac46adc8c732eef6a6e5857c9f3c
emanuel.alexandre <emanuel.melo87@gmail.com>
2021-09-18 10:07:01 -0300

    Adição inicial

--- src/main/java/com/betha/statustce/statustce/resource/PaisController.java
+++ src/main/java/com/betha/statustce/statustce/resource/PaisController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.persistence.EntityNotFoundException;
 import javax.xml.ws.Response;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/paises")
@@ -19,19 +20,19 @@ public class PaisController {
     private PaisRepository repository;
 
     @GetMapping
-    public List<Pais> getPaises(){
-        return repository.findAll();
+    public List<PaisDTO> getPaises(){
+        return repository.findAll().stream().map(p-> PaisDTO.toDTO(p)).collect(Collectors.toList());
     }
 
     @GetMapping("/{id}")
-    public Pais getPaisesId(@PathVariable(value = "id") Long paisId) throws EntityNotFoundException{
+    public PaisDTO getPaisesId(@PathVariable(value = "id") Long paisId) throws EntityNotFoundException{
         Pais paisFind = repository.findById(paisId).orElseThrow(() -> new EntityNotFoundException("País não encontado com o ID"+ paisId));
-        return paisFind;
+        return PaisDTO.toDTO(paisFind);
     }
 
     @PostMapping
-    public Pais create(@RequestBody Pais pais){
-        return repository.save(pais);
+    public PaisDTO create(@RequestBody Pais pais){
+        return PaisDTO.toDTO(repository.save(pais));
     }
 
     @PutMapping("/{id}")
