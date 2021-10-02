6b055220f5fd6ed5fdba8689fd7d742421c6fdd8
emanuel.alexandre <emanuel.melo87@gmail.com>
2021-10-02 10:11:39 -0300

    Adição inicial

---
+++ src/main/java/com/betha/statustce/statustce/repository/AssuntoRepository.java
@@ -0,0 +1,12 @@
+package com.betha.statustce.statustce.repository;
+
+import com.betha.statustce.statustce.model.Competencia;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface AssuntoRepository extends JpaRepository<Competencia, Long> {
+
+}
+
---
+++ src/main/java/com/betha/statustce/statustce/repository/EmpresaAtendimentoRepository.java
@@ -0,0 +1,11 @@
+package com.betha.statustce.statustce.repository;
+
+import com.betha.statustce.statustce.model.Competencia;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface EmpresaAtendimentoRepository extends JpaRepository<Competencia, Long> {
+
+}
