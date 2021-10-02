d0fcdcaea4bf74f0c2b0ab459c727b23ac0e25a7
emanuel.alexandre <emanuel.melo87@gmail.com>
2021-10-02 11:00:35 -0300

    Adição inicial

---
+++ src/main/java/com/betha/statustce/statustce/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.betha.statustce.statustce.repository;
+
+import com.betha.statustce.statustce.model.Competencia;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Competencia, Long> {
+
+}
