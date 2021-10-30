de596e5d396a2dead91f929db81cfaf69df02788
emanuel.alexandre <emanuel.melo87@gmail.com>
2021-10-30 11:36:17 -0300

    eMANUEL

--- src/main/java/com/betha/statustce/statustce/resource/UnidadeAtendimentoController.java
+++ src/main/java/com/betha/statustce/statustce/resource/UnidadeAtendimentoController.java
@@ -21,7 +21,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/unidadeatendimento    ")
+@RequestMapping("/api/unidadeatendimento")
 
 public class UnidadeAtendimentoController {
     @Autowired
