8ad80bf98f3a0120d250712910576a1c6b7e6cc7
emanuel.alexandre <emanuel.melo87@gmail.com>
2021-10-30 08:48:45 -0300

    Adição inicial

---
+++ src/main/java/com/betha/statustce/statustce/enterprise/ValidationException.java
@@ -0,0 +1,14 @@
+package com.betha.statustce.statustce.enterprise;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY)
+public class ValidationException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public ValidationException(String message) {
+        super(message);
+    }
+}
